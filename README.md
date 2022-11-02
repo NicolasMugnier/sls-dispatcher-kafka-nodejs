a7bc27a5b994ce580be0c4e7f2d90c212e566ce9
Nicolas Mugnier <6680190+NicolasMugnier@users.noreply.github.com>
2022-11-02 19:22:06 +0100

    feat(broker-dispatcher): Create broker dispatcher

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # sls-dispatcher-kafka-nodejs
+
+## Install
+
+```shell
+npm install
+```
+
+## Start offline
+
+```shell
+sls offline start
+```
+
+## Example
+
+```shell
+curl -XPOST {{url}}/dev/message/some-topic -d '{"key": "value", "item": {"name": "awesome"}}'
+```
---
+++ src/api/dispatcher.ts
@@ -0,0 +1,38 @@
+import { Handler, Context, APIGatewayEvent } from 'aws-lambda';
+import { v4 as uuid } from 'uuid';
+import { Kafka } from 'kafkajs';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+interface Response {
+    statusCode: number;
+    body: string;
+}
+
+const kafka = new Kafka({
+    brokers: process.env.BROKERS?.split(',') || [],
+});
+
+const producer = kafka.producer();
+
+export const handler: Handler = async (event: APIGatewayEvent, context: Context, callback): Promise<Response> => {
+
+    const pathParameters = event.pathParameters;
+    const topic: string = pathParameters?.topic || 'default-topic';
+    console.log(event.pathParameters);
+    await producer.connect();
+    await producer.send({
+        topic,
+        messages: [
+            { value: event.body },
+        ],
+    });
+
+    return Promise.resolve({
+        statusCode: 202,
+        body: JSON.stringify({
+          "asyncOperationId": uuid()
+        }),
+      });
+};
